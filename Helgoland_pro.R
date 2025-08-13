745a4bd3f2ef39c3ad5624ef73a12eff01ce0a8b
Farhah Alkaff <sharifahfarhah5@gmail.com>
2025-08-14 00:11:20 +1000

    plotting changes in higher moments from data

--- Helgoland_pro.R
+++ Helgoland_pro.R
@@ -35,6 +35,8 @@ H98 <- read.csv("dataset/Helgoland_1998.csv")
 H2002 <- read.csv("dataset/Helgoland_2002.csv")
 
 library(dplyr)
+library(ggplot2)
+library(patchwork)
 
 # Combine all datasets into one
 df <- bind_rows(H62, H63, H64, H65, H66, H67, H68, H69, H70, H71, H72, H73, H74, H75, H76,
@@ -45,4 +47,87 @@ library(lubridate)
 
 # add year as a column
 df$year <- year(ymd_hm(df$Date))
+
+# See changes in temp throughout the years 
+## Calculate yearly average temperature
+yearly_avg <- df %>%
+  group_by(year) %>%
+  summarise(avg_temp = mean(Temp, na.rm = TRUE))
+### plot the average temp for each year
+ggplot(yearly_avg, aes(x = year, y = avg_temp)) +
+  geom_line() +
+  geom_point() +
+  labs(title = "Average Temperature per Year",
+       x = "Year",
+       y = "Average Temperature")
+
+
+# see how nitrite differ over the years 
+library(dplyr)
+library(e1071)  # for skewness and kurtosis
+## variance
+yearly_stats <- df %>%
+  group_by(year) %>%
+  summarise(
+    mean_nitrite = mean(Nitrite, na.rm = TRUE),
+    var_nitrite = var(Nitrite, na.rm = TRUE),
+    skew_nitrite = skewness(Nitrite, na.rm = TRUE),
+    kurt_nitrite = kurtosis(Nitrite, na.rm = TRUE),
+    mean_nitrate = mean(Nitrate, na.rm = TRUE),
+    var_nitrate = var(Nitrate, na.rm = TRUE),
+    skew_nitrate = skewness(Nitrate, na.rm = TRUE),
+    kurt_nitrate = kurtosis(Nitrate, na.rm = TRUE),
+    mean_phospate = mean(Phosphate, na.rm = TRUE),
+    var_phosphate = var(Phosphate, na.rm = TRUE),
+    skew_phosphate = skewness(Phosphate, na.rm = TRUE),
+    kurt_phosphate = kurtosis(Phosphate, na.rm = TRUE)
+  )
+##visualize for variance 
+ni_v <- ggplot(yearly_stats, aes(x = year)) +
+  geom_line(aes(y = var_nitrite), color = 'blue') +
+  labs(title = "Variance of Nitrite Over Years", y = "Variance")
+##visualize for skewness 
+ni_s <- ggplot(yearly_stats, aes(x = year)) +
+  geom_line(aes(y = skew_nitrite), color = 'blue') +
+  labs(title = "Skewness of Nitrite Over Years", y = "Skewness")
+##visualize for kurtosis 
+ni_k <- ggplot(yearly_stats, aes(x = year)) +
+  geom_line(aes(y = kurt_nitrite), color = 'blue') +
+  labs(title = "Kurtosis of Nitrite Over Years", y = "Kurtosis")
+###stack them together 
+(ni_v / ni_s / ni_k) + plot_layout(ncol = 1)
+
+
+# see how nitrate differ over the years 
+##visualize for variance 
+na_v <- ggplot(yearly_stats, aes(x = year)) +
+  geom_line(aes(y = var_nitrate), color = 'goldenrod') +
+  labs(title = "Variance of Nitrate Over Years", y = "Variance")
+##visualize for skewness 
+na_s <- ggplot(yearly_stats, aes(x = year)) +
+  geom_line(aes(y = skew_nitrate), color = 'goldenrod') +
+  labs(title = "Skewness of Nitrate Over Years", y = "Skewness")
+##visualize for kurtosis 
+na_k <- ggplot(yearly_stats, aes(x = year)) +
+  geom_line(aes(y = kurt_nitrate), color = 'goldenrod') +
+  labs(title = "Kurtosis of Nitrate Over Years", y = "Kurtosis")
+###stack them together 
+(na_v / na_s / na_k) + plot_layout(ncol = 1)
+
+
+# see how phosphate differ over the years 
+##visualize for variance 
+p_v <- ggplot(yearly_stats, aes(x = year)) +
+  geom_line(aes(y = var_phosphate), color = 'darkolivegreen') +
+  labs(title = "Variance of Phosphate Over Years", y = "Variance")
+##visualize for skewness 
+p_s <- ggplot(yearly_stats, aes(x = year)) +
+  geom_line(aes(y = skew_phosphate), color = 'darkolivegreen') +
+  labs(title = "Skewness of Phosphate Over Years", y = "Skewness")
+##visualize for kurtosis 
+p_k <- ggplot(yearly_stats, aes(x = year)) +
+  geom_line(aes(y = kurt_phosphate), color = 'darkolivegreen') +
+  labs(title = "Kurtosis of Phosphate Over Years", y = "Kurtosis")
+###stack them together 
+(p_v / p_s / p_k) + plot_layout(ncol = 1)
 
